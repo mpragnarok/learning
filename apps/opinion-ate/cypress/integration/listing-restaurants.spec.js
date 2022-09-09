793cfbbb42ebbc61d1a78b76580ef0fd1c835344
minahuang <minahuang@jubo.health>
2022-09-09 16:51:34 +0800

    feat(listing-restaurants.spec): specify app should list restaurants

---
+++ apps/opinion-ate/cypress/integration/listing-restaurants.spec.js
@@ -0,0 +1,13 @@
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+    cy.intercept('GET', 'https://api.outsidein.dev/*/restaurants', [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'pizzaPlace'},
+    ]);
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
--- apps/opinion-ate/src/App.js
+++ apps/opinion-ate/src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 export default function App() {
   return <div>Hello, world.</div>;
 }
